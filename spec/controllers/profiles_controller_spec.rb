b28235447e8b8daae6307e138fe2e2686830e21d
Piet Brauer <piet.brauer@xing.com>
2013-12-20 14:43:24 +0100

    Add index action tests

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -1,4 +1,26 @@
 require 'spec_helper'
 
 describe ProfilesController do
+
+  describe "index" do
+    it "responds successfully" do
+      get :index
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "loads all profiles" do
+      profile1 = FactoryGirl.create(:profile)
+      profile2 = FactoryGirl.create(:profile)
+
+      get :index
+      expect(assigns(:profiles)).to include(profile1)
+      expect(assigns(:profiles)).to include(profile2)
+    end
+  end
 end
