fb7ba4f374feabc1fe8eb3398d131931441a08bc
fabiansch <stahlschiff@web.de>
2013-12-20 14:39:10 +0100

    change db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20131219181702) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.string   "gravatar_email"
   end
 
