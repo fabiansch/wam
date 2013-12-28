96a896045c27eb66474669fa81ca9364e7d5aff7
Piet Brauer <piet.brauer@xing.com>
2013-12-28 17:44:24 +0100

    Add "I would like to" fields to profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131227162052) do
+ActiveRecord::Schema.define(version: 20131228161702) do
 
   create_table "profiles", force: true do |t|
     t.string   "name"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 20131227162052) do
     t.string   "twitter_handle"
     t.string   "xing_handle"
     t.string   "linkedin_handle"
+    t.boolean  "meet_for_coffee"
+    t.boolean  "attend_meetings"
+    t.boolean  "be_mentor"
+    t.string   "own_like_string"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
